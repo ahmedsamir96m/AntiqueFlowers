581647fa5d552b2f59812f31a48475c05aa2d4f5
Ahmad Samir <ahmedsamir96m@gmail.com>
2020-03-28 01:45:21 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './scss/main.scss';
 
 console.log(a8);
 
+console.log('hi')
---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', //3
+          'css-loader', //2
+          'sass-loader', //1
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name]-[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "fonts"
+          }
+        }
+      }
+    ]
+  }
+}
